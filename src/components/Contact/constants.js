984cdd559223e24354e263af1a6e7be43c4ae438
Stephany Nusch <stephany.nusch@quintoandar.com.br>
2020-08-09 16:27:35 -0300

    uploading first version

---
+++ src/components/Contact/constants.js
@@ -0,0 +1,18 @@
+export const SOCIAL_MEDIA = [
+    {
+        name: 'LinkedIn',
+        url: 'https://www.linkedin.com/in/stephanynusch/',
+    },
+    {
+        name: 'Twitter',
+        url: 'https://twitter.com/stebsnusch',
+    },
+    {
+        name: 'Github',
+        url: 'https://github.com/stebsnusch',
+    },
+    {
+        name: 'E-mail',
+        url: 'mailto:stephanyn7@gmail.com',
+    },
+]
